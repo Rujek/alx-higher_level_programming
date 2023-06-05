9dbaff9758d9cfac9b7221fc161d89fa9cf9d02d
Rujek <rujeko24@gmail.com>
2023-06-05 11:31:08 -0700

    Linked lists

--- 0x00-python-hello_world/10-linked_lists.c
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -1,18 +1,19 @@
 #include "lists.h"
 
 /**
- * check_cycle -checks if a linked list contains a cycle
+ * check_cycle - checks if a linked list contains a cycle
  * @list: linked list to check
- * Return: 1 if theblist has a cycle, 0 if it does not
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-
 int check_cycle(listint_t *list)
 {
 	listint_t *slow = list;
-	listint_t *fast = list
+	listint_t *fast = list;
 
 	if (!list)
 		return (0);
+
 	while (slow && fast && fast->next)
 	{
 		slow = slow->next;
@@ -20,5 +21,6 @@ int check_cycle(listint_t *list)
 		if (slow == fast)
 			return (1);
 	}
+
 	return (0);
 }
