4df2c6f97cd6ee1b91f8870699a114b633bac44d
Rujek <rujeko24@gmail.com>
2023-09-30 13:05:21 -0700

    Status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""make requests
-"""
+"""Fetches https://intranet.hbtn.io/status."""
+import urllib.request
+
 
 if __name__ == "__main__":
-    import urllib.request as url
-    req = url.Request('https://intranet.hbtn.io/status')
-    with url.urlopen(req) as response:
-        html = response.read()
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type:", type(html))
-        print("\t- content:", html)
-        print("\t- utf8 content:", html.decode('utf-8'))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
